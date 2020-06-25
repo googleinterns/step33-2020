bbe0622188975d6a75195f85995b6e07ea999b5d
juliareichel <juliareichel@google.com>
2020-06-25 15:45:27 -0400

    Created initial button with ad param.

--- src/main/webapp/creatives/maps/simid_maps.js
+++ src/main/webapp/creatives/maps/simid_maps.js
@@ -5,4 +5,25 @@ class SimidMapCreative extends BaseSimidCreative {
   constructor() {
     super();
   }
+  /** @override */
+  onStart(eventData){
+    super.onStart(eventData);
+    this.adParams = JSON.parse(this.creativeData.adParameters);
+    console.log("Ad params: ", this.adParams);
+    var paramObject = this.adParams[0];
+    console.log(paramObject);
+    var buttonParam = paramObject["buttonParam"];
+    console.log(buttonParam);
+    this.specifyButtonText(buttonParam);
+  }
+ 
+  specifyButtonText(buttonParam) {
+    var findNearestButton = document.getElementById('findNearest');
+    findNearestButton.innerText = "Find Nearest " + buttonParam;
+    findNearest.onclick = () => displayMap();
+  }
+ 
+  displayMap() {
+  //To Do -- Kristen implement this 
+  }
 }
