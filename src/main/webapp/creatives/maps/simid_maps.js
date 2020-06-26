a31d3c08f563358c69f3a6cbe11a26a4910e4eaf
Daniel Margulis <damargulis1@gmail.com>
2020-06-26 10:42:50 -0400

    init code back to html

--- src/main/webapp/creatives/maps/simid_maps.js
+++ src/main/webapp/creatives/maps/simid_maps.js
@@ -4,11 +4,8 @@ import BaseSimidCreative from '../base_simid_creative.js';
 /**
  * A sample SIMID ad that shows a map of nearby locations
  */
-class SimidMapCreative extends BaseSimidCreative {
+export default class SimidMapCreative extends BaseSimidCreative {
   constructor() {
     super();
   }
 }
-
-const map = new SimidMapCreative();
-map.ready();
