43e3f914cc757374b8ed5e2906385618c99a5101
Kaleab G <kaleabsolomon470@gmail.com>
2020-06-26 14:22:55 -0500

    Add javadoc explaining grantlocation route

--- src/main/java/com/google/sps/servlets/GrantLocationServlet.java
+++ src/main/java/com/google/sps/servlets/GrantLocationServlet.java
@@ -9,6 +9,12 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/grant-location")
 public class GrantLocationServlet extends HttpServlet {
 
+ /**
+  * Given the correlator, this will update the database to reflect that the user allowed their location to be accessed.
+  *
+  * @param  request  This includes a correlator for the current user.
+  * @param  response An empty 200 response 
+  */
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
     response.setContentType("text/html;");
