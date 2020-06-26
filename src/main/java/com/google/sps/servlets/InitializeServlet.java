4ce00a4e499178080805f10ab8d37d0cc87e596a
Kaleab G <kaleabsolomon470@gmail.com>
2020-06-26 14:21:55 -0500

    Add javadoc explaining initialize route

--- src/main/java/com/google/sps/servlets/InitializeServlet.java
+++ src/main/java/com/google/sps/servlets/InitializeServlet.java
@@ -10,6 +10,12 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/initialize")
 public class InitializeServlet extends HttpServlet {
 
+ /**
+  * Given the correlator, this will create an entry in the database with default values.
+  *
+  * @param  request  This includes a correlator for the current user.
+  * @param  response An empty 200 response 
+  */
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
     response.setContentType("text/html;");
