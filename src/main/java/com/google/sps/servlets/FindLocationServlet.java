8c93477859bef8e4b205448f13b8ab76a37d2ab4
Kaleab G <kaleabsolomon470@gmail.com>
2020-06-26 11:17:53 -0500

    Add FindLocation servlet

--- src/main/java/com/google/sps/servlets/FindLocationServlet.java
+++ src/main/java/com/google/sps/servlets/FindLocationServlet.java
@@ -1,4 +1,3 @@
-
 package com.google.sps.servlets;
 
 import java.io.IOException;
@@ -7,13 +6,13 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet("/initialize")
-public class InitializeServlet extends HttpServlet {
+@WebServlet("/findLocation")
+public class FindLocationServlet extends HttpServlet {
 
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
     response.setContentType("text/html;");
-    response.getWriter().println("<h1>Initialize</h1>");
+    response.getWriter().println("<h1>Find Location</h1>");
   }
 
   @Override
