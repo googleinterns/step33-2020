f34a50379ce747cc4103572f649d8ba2e44aeb50
juliareichel <juliareichel@google.com>
2020-06-26 14:04:26 -0400

    Function descriptions, default params, small nits.

--- src/main/webapp/creatives/maps/simid_maps.js
+++ src/main/webapp/creatives/maps/simid_maps.js
@@ -5,7 +5,9 @@ const AdParamKeys = {
   BUTTON_LABEL: 'buttonLabel',
   SEARCH_QUERY: 'searchQuery',
   MARKER: 'marker',
-}
+};
+
+const FIND_NEAREST = "Find Nearest ";
 const DEFAULT_BUTTON_LABEL = "Location";
 
 class SimidMapCreative extends BaseSimidCreative {
@@ -16,29 +18,30 @@ class SimidMapCreative extends BaseSimidCreative {
   /** @override */
   onStart(eventData){
     super.onStart(eventData);
-    const adParams = this.creativeData.adParameters;
-    const buttonLabel = JSON.parse(adParams)[AdParamKeys.BUTTON_LABEL]; 
-    if (buttonLabel == undefined) {
-      buttonLabel = DEFAULT_BUTTON_LABEL;
-    }
-    else {
-      buttonLabel = JSON.parse(adParams)[AdParamKeys.BUTTON_LABEL];
-    }
-    let searchQuery = JSON.parse(adParams)[AdParamKeys.SEARCH_QUERY];
+    const adParams = JSON.parse(this.creativeData.adParameters);
+    let buttonLabel = adParams[AdParamKeys.BUTTON_LABEL]; 
+    //ToDo(juliareichel@): handle invalid JSON and param errors
+    const searchQuery = adParams[AdParamKeys.SEARCH_QUERY];
     //ToDo(juliareichel@): handle case where searchQuery is undefined
-    const marker = JSON.parse(adParams)[AdParamKeys.MARKER];;
+    const marker = adParams[AdParamKeys.MARKER];
     this.specifyButtonFeatures_(buttonLabel);
   }
  
-  /** @private */
-  specifyButtonFeatures_(buttonLabel) {
+  /** @private 
+   * Sets the text of the initial button and assigns it a click functionality.
+   * @param {string=} buttonLabel refers to the default value "location" written on the
+   * Find Nearest button. Advertisers have the option of changing the text on button.
+  */   
+  specifyButtonFeatures_(buttonLabel = DEFAULT_BUTTON_LABEL) {
     const findNearestButton = document.getElementById('findNearest');
-    findNearestButton.innerText = "Find Nearest " + buttonLabel;
-    findNearest.onclick = () => this.displayMap_();
+    findNearestButton.innerText = FIND_NEAREST + buttonLabel;
+    findNearest.onclick = () => this.grantLocationAccess_();
   }
  
-  /** @private */
-  displayMap_() {
-  //ToDo(kristenmason@): implement map
+  /** @private 
+   * Prompts the users to grant or deny access to their current location.
+  */
+  grantLocationAccess_() {
+    //ToDo(kristenmason@): implement map
   }
 }
