d01ac5115c15442e811b6f79af213048ea7bb176
Kaleab G <kaleabsolomon470@gmail.com>
2020-06-26 11:19:12 -0500

    Add GrantLocation servlet

--- src/main/java/com/google/sps/servlets/GrantLocationServlet.java
+++ src/main/java/com/google/sps/servlets/GrantLocationServlet.java
@@ -6,18 +6,18 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet("/findLocation")
-public class FindLocationServlet extends HttpServlet {
+@WebServlet("/grantLocation")
+public class InitializeServlet extends HttpServlet {
 
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
     response.setContentType("text/html;");
-    response.getWriter().println("<h1>Find Location</h1>");
+    response.getWriter().println("<h1>Grant Location</h1>");
   }
 
   @Override
   public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
 
-    response.sendRedirect("/findLocation");
+    response.sendRedirect("/grantLocation");
   }
 }
