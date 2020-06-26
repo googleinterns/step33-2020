aa63783f027dc876848e04ed715fe9e0c148d9ba
Kaleab G <kaleabsolomon470@gmail.com>
2020-06-26 14:16:39 -0500

    Add javadoc explaining Skiptocontent route

--- src/main/java/com/google/sps/servlets/SkipToContentServlet.java
+++ src/main/java/com/google/sps/servlets/SkipToContentServlet.java
@@ -9,6 +9,12 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/skip-to-content")
 public class SkipToContentServlet extends HttpServlet {
 
+  /**
+  * Given the correlator, this will update the database to reflect that the user clicked 'skip to content'.
+  *
+  * @param  request  This includes a correlator for the current user.
+  * @param  response An empty 200 response 
+  */
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
     response.setContentType("text/html;");
