05c3d41df87149f19b203a46a67ef4d1a01b15cc
Kaleab G <kaleabsolomon470@gmail.com>
2020-06-26 14:20:16 -0500

    Add javadoc explaining interactwithmap route

--- src/main/java/com/google/sps/servlets/MapInteractServlet.java
+++ src/main/java/com/google/sps/servlets/MapInteractServlet.java
@@ -9,6 +9,12 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/map-interact")
 public class MapInteractServlet extends HttpServlet {
 
+ /**
+  * Given the correlator, this will update the database to reflect that the user interacted with the map.
+  *
+  * @param  request  This includes a correlator for the current user.
+  * @param  response An empty 200 response 
+  */
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
     response.setContentType("text/html;");
