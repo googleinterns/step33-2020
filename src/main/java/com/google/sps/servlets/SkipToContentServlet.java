80f41c89454341f1855536993b23aeb88c88fc26
Kaleab G <kaleabsolomon470@gmail.com>
2020-06-26 11:22:07 -0500

    Add SkiptoContent servlet

---
+++ src/main/java/com/google/sps/servlets/SkipToContentServlet.java
@@ -0,0 +1,23 @@
+package com.google.sps.servlets;
+
+import java.io.IOException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/skipToContent")
+public class InitializeServlet extends HttpServlet {
+
+  @Override
+  public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    response.setContentType("text/html;");
+    response.getWriter().println("<h1>Skip to Content</h1>");
+  }
+
+  @Override
+  public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+
+    response.sendRedirect("/skipToContent");
+  }
+}
