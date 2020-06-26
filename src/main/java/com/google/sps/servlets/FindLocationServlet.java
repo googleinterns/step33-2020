fd4cd8422b888d7315e02ae81ffe4ccdaf28892b
Kaleab G <kaleabsolomon470@gmail.com>
2020-06-26 14:23:34 -0500

    Add javadoc explaining findlocation route

--- src/main/java/com/google/sps/servlets/FindLocationServlet.java
+++ src/main/java/com/google/sps/servlets/FindLocationServlet.java
@@ -9,6 +9,12 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/find-location")
 public class FindLocationServlet extends HttpServlet {
 
+ /**
+  * Given the correlator, this will update the database to reflect that the user clicked 'fiind nearby location'.
+  *
+  * @param  request  This includes a correlator for the current user.
+  * @param  response An empty 200 response 
+  */
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
     response.setContentType("text/html;");
