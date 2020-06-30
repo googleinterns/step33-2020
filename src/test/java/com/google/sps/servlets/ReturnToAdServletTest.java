e8ceaa14bfa9e7d38dcb5fc6ef80f1bc0ac23bbe
Kaleab G <kaleabsolomon470@gmail.com>
2020-06-30 11:45:31 -0500

    Add test for return to ad servlet

---
+++ src/test/java/com/google/sps/servlets/ReturnToAdServletTest.java
@@ -0,0 +1,48 @@
+package com.google.sps.servlets;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+import com.google.sps.servlets.DBUtilities;
+import org.mockito.Mock;
+
+@RunWith(JUnit4.class)
+public final class ReturnToAdServletTest {
+
+  @Test
+  public void testCorrectStatusSent() {
+    HttpServletRequest request = mock(HttpServletRequest.class);       
+    HttpServletResponse response = mock(HttpServletResponse.class);    
+
+    when(request.getParameter("correlator")).thenReturn("Person1");
+
+    new ReturnToAdServlet().doGet(request, response);
+
+    assertEquals(HttpServletResponse.SC_OK, response.getStatus());
+  }
+
+  @Test
+  public void checkIfBadRequestStatusSentNull() {
+    HttpServletRequest request = mock(HttpServletRequest.class);       
+    HttpServletResponse response = mock(HttpServletResponse.class);    
+
+    when(request.getParameter("correlator")).thenReturn(null);
+
+    new ReturnToAdServlet().doGet(request, response);
+
+    assertEquals(HttpServletResponse.SC_BAD_REQUEST, response.getStatus());
+  }
+
+  @Test
+  public void checkIfBadRequestStatusSentEmpty() {
+    HttpServletRequest request = mock(HttpServletRequest.class);       
+    HttpServletResponse response = mock(HttpServletResponse.class);    
+
+    when(request.getParameter("correlator")).thenReturn("");
+
+    new ReturnToAdServlet().doGet(request, response);
+
+    assertEquals(HttpServletResponse.SC_BAD_REQUEST, response.getStatus());
+  }
+}
