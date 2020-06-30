c5eae8e29197313a22c54df3679b072188929396
Kaleab G <kaleabsolomon470@gmail.com>
2020-06-30 11:20:01 -0500

    Finish tests for initialize servlet

--- src/test/java/com/google/sps/servlets/InitializeServletTest.java
+++ src/test/java/com/google/sps/servlets/InitializeServletTest.java
@@ -1,35 +1,17 @@
 package com.google.sps.servlets;
 
 import org.junit.Assert;
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
-import com.google.appengine.tools.development.testing.LocalDatastoreServiceTestConfig;
-import com.google.appengine.tools.development.testing.LocalServiceTestHelper;
 import com.google.sps.servlets.DBUtilities;
-import com.google.sps.servlets.Property;
 import org.mockito.Mock;
-import org.mockito.runners.MockitoJUnitRunner;
 
 @RunWith(JUnit4.class)
 public final class InitializeServletTest {
 
-  private final LocalServiceTestHelper helper =
-      new LocalServiceTestHelper(new LocalDatastoreServiceTestConfig());
-
-  @Before
-  public void setUp() {
-    helper.setUp();
-  }
-
-  @After
-  public void tearDown() {
-    helper.tearDown();
-  }
-
   @Test
-  public void testIfOnlyOneEntryMade() {
+  public void testCorrectStatusSent() {
     HttpServletRequest request = mock(HttpServletRequest.class);       
     HttpServletResponse response = mock(HttpServletResponse.class);    
 
@@ -37,10 +19,11 @@ public final class InitializeServletTest {
 
     new InitializeServlet().doGet(request, response);
 
+    assertEquals(HttpServletResponse.SC_OK, response.getStatus());
   }
 
   @Test
-  public void checkIfBadRequestStatusSent() {
+  public void checkIfBadRequestStatusSentNull() {
     HttpServletRequest request = mock(HttpServletRequest.class);       
     HttpServletResponse response = mock(HttpServletResponse.class);    
 
@@ -52,27 +35,14 @@ public final class InitializeServletTest {
   }
 
   @Test
-  public void checkIfNothingUpdatesWhenPersonNotFound() {
-    DatastoreService datastore = setDatabaseUp();
-    
-    DBUtilities.setToTrue("Person2", Property.GRANTS_LOCATION);
+  public void checkIfBadRequestStatusSentEmpty() {
+    HttpServletRequest request = mock(HttpServletRequest.class);       
+    HttpServletResponse response = mock(HttpServletResponse.class);    
 
-    Entity originalInteraction = new Entity("Interactions");
-    originalInteraction.setProperty(Property.CORRELATOR, "Person1");
-    originalInteraction.setProperty(Property.FIND_NEAREST_LOCATION, false);
-    originalInteraction.setProperty(Property.GRANTS_LOCATION, false);
-    originalInteraction.setProperty(Property.INTERACTS_WITH_MAP, false);
-    originalInteraction.setProperty(Property.SKIP_TO_CONTENT, false);
-    originalInteraction.setProperty(Property.RETURN_TO_AD, false);
+    when(request.getParameter("correlator")).thenReturn("");
 
-    final Filter correlatorFilter =  new FilterPredicate(Property.CORRELATOR, FilterOperator.EQUAL, "Person1");
-    final Query interactionQuery = new Query(INTERACTION_TABLE).setFilter(correlatorFilter);
+    new InitializeServlet().doGet(request, response);
 
-    DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
-    PreparedQuery filteredImpression = datastore.prepare(interactionQuery);
-
-    Entity currentInteraction = filteredImpression.asSingleEntity(); 
-
-    assertEquals(originalInteraction.toString(), currentInteraction.toString());
+    assertEquals(HttpServletResponse.SC_BAD_REQUEST, response.getStatus());
   }
 }
