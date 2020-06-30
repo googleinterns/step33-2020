1756a2c24d5a5a54572cbe8c44630d8708d47792
Kaleab G <kaleabsolomon470@gmail.com>
2020-06-30 11:58:53 -0500

    Add test for RequestUtil

---
+++ src/test/java/com/google/sps/servlets/RequestUtilsTest.java
@@ -0,0 +1,40 @@
+package com.google.sps.servlets;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+import com.google.sps.servlets.DBUtilities;
+import org.mockito.Mock;
+import com.google.sps.servlets.RequestUtils;
+
+@RunWith(JUnit4.class)
+public final class RequestUtilsTest {
+
+  @Test
+  public void testCorrelatorValid() {
+    HttpServletRequest request = mock(HttpServletRequest.class);    
+
+    when(request.getParameter("correlator")).thenReturn("Person1");
+    
+    assertEquals(RequestUtils.getCorrelator(request), "Person1");
+  }
+
+  @Test
+  public void testCorrelatorInvalid() {
+    HttpServletRequest request = mock(HttpServletRequest.class);    
+
+    when(request.getParameter("correlator")).thenReturn("");
+    
+    assertEquals(RequestUtils.getCorrelator(request), "");
+  }
+
+  @Test
+  public void testCorrelatorValid() {
+    HttpServletRequest request = mock(HttpServletRequest.class);    
+
+    when(request.getParameter("correlator")).thenReturn(null);
+    
+    assertEquals(RequestUtils.getCorrelator(request), "");
+  }
+}
