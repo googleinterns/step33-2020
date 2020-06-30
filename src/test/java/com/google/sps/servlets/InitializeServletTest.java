50fa05d8f5856025d1daf37c52e6d2e5b59f23fb
Kaleab G <kaleabsolomon470@gmail.com>
2020-06-30 10:14:54 -0500

    Add test file for initialize servlet

---
+++ src/test/java/com/google/sps/servlets/InitializeServletTest.java
@@ -0,0 +1,12 @@
+package com.google.sps.servlets;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+import com.google.appengine.tools.development.testing.LocalDatastoreServiceTestConfig;
+import com.google.appengine.tools.development.testing.LocalServiceTestHelper;
+import com.google.sps.servlets.DBUtilities;
+import com.google.sps.servlets.Property;
+
