63608aefa04444f89da055e2e5ddb10da3e1e366
kristen-m <kristenmason@google.com>
2020-07-01 15:38:36 -0500

    Add test for loadMap

--- src/main/webapp/creatives/maps/simid_map_creative.test.js
+++ src/main/webapp/creatives/maps/simid_map_creative.test.js
@@ -1,12 +1,22 @@
 /** Tests to see if Find Nearest X button is appending correct text */
-
+import '@testing-library/jest-dom';
 import SimidMapCreative from './simid_map_creative.js';
 
-let testMap;
+let testMap, googleMaps;
 
 beforeEach(() => {
+    window.google = {
+        maps: {
+            LatLng: jest.fn(), 
+            Map: jest.fn(),
+            Marker: jest.fn(),
+            Autocomplete: class {}
+        }
+      };
     testMap = new SimidMapCreative();
-    document.body.innerHTML = '<button id="findNearest"></button>';
+    document.body.innerHTML = `
+    <button id="findNearest"></button>
+    <div id="map"></div>`;
 });
 
 test('testing button text updates from ad params', () => {
@@ -63,3 +73,32 @@ test('testing button text updates with default ad params', () => {
     expect(button.innerText).toBe('Find Nearest Location');
 });
 
+test('Map constructor in Maps API is accessed on button click', () => {
+    const eventData = {
+        args: {
+            creativeData: {
+                adParameters: '{"buttonLabel": "Place"}',
+            },
+            environmentData: {},
+        },
+        messageId: 0,
+        sessionId: "test-session-id",
+        timestamp: 0,
+        type: "SIMID:Player:init",
+    };
+    testMap.onInit(eventData);
+
+    const startData = {
+        messageId: 1,
+        sessionId: "test-session-id",
+        timestamp: 0,
+        type: "SIMID:Player:startCreative",  
+    }
+    testMap.onStart(startData);
+
+    const findNearestButton = document.getElementById('findNearest');
+    const mapDiv = document.getElementById("map");
+    findNearestButton.dispatchEvent(new Event('click'));
+    expect(window.google.maps.Map.mock.instances.length).toBe(1);
+
+});
