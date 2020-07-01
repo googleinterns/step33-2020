8bc2355dc97f3fb1eaa5dedab496771044c3d26f
juliareichel <juliareichel@google.com>
2020-07-01 16:38:26 -0400

    Added tests that check for proper reject message.

--- src/main/webapp/creatives/maps/simid_map_creative.test.js
+++ src/main/webapp/creatives/maps/simid_map_creative.test.js
@@ -1,10 +1,13 @@
 /** Tests to see if Find Nearest X button is appending correct text */
 
 import SimidMapCreative from './simid_map_creative.js';
+import SimidProtocol from '../simid_protocol.js';
 
+jest.mock('../simid_protocol.js');
 let testMap;
 
 beforeEach(() => {
+    SimidProtocol.mockClear();
     testMap = new SimidMapCreative();
     document.body.innerHTML = '<button id="findNearest"></button>';
 });
@@ -62,5 +65,87 @@ test('testing button text updates with default ad params', () => {
 
     const button = document.getElementById("findNearest");
     expect(button.innerText).toBe('Find Nearest Location');
+});
+
+test('testing that rejection for ad params not found is working', () => {
+    const eventData = {
+        args: {
+            creativeData: {
+                adParameters: "",
+            },
+            environmentData: {},
+        },
+        messageId: 0,
+        sessionId: "test-session-id",
+        timestamp: 0,
+        type: "SIMID:Player:init",
+    };
+    testMap.onInit(eventData);
+    
+    const instance = SimidProtocol.mock.instances[0];
+    const rejectMessageObject = instance.reject.mock.calls[0][1];
+    expect(rejectMessageObject.message).toBe("Ad parameters not found");
+});
+
+test('testing that rejection for searchQuery not found is working when given empty string', () => {
+    const eventData = {
+        args: {
+            creativeData: {
+                adParameters: '{"searchQuery": ""}',
+            },
+            environmentData: {},
+        },
+        messageId: 0,
+        sessionId: "test-session-id",
+        timestamp: 0,
+        type: "SIMID:Player:init",
+    };
+    testMap.onInit(eventData);
+    
+    const instance = SimidProtocol.mock.instances[0];
+    expect(instance.reject).toHaveBeenCalledTimes(1);
+    const rejectMessageObject = instance.reject.mock.calls[0][1];
+    expect(rejectMessageObject.message).toBe("Required field searchQuery not found");
+});
+
+test('testing that rejection for searchQuery not found is working when search query not included', () => {
+    const eventData = {
+        args: {
+            creativeData: {
+                adParameters: '{"buttonLabel": "Location", "marker": "https://upload.wikimedia.org/wikipedia/en/thumb/d/d3/Starbucks_Corporation_Logo_2011.svg.png"}'
+            },
+            environmentData: {},
+        },
+        messageId: 0,
+        sessionId: "test-session-id",
+        timestamp: 0,
+        type: "SIMID:Player:init",
+    };
+    testMap.onInit(eventData);
+    
+    const instance = SimidProtocol.mock.instances[0];
+    expect(instance.reject).toHaveBeenCalledTimes(1);
+    const rejectMessageObject = instance.reject.mock.calls[0][1];
+    expect(rejectMessageObject.message).toBe("Required field searchQuery not found");
+});
+
+test('testing that rejection for JSON parsing errors is working', () => {
+    const eventData = {
+        args: {
+            creativeData: {
+                adParameters: 'test',
+            },
+            environmentData: {},
+        },
+        messageId: 0,
+        sessionId: "test-session-id",
+        timestamp: 0,
+        type: "SIMID:Player:init",
+    };
+    testMap.onInit(eventData);
+    
+    const instance = SimidProtocol.mock.instances[0];
+    const rejectMessageObject = instance.reject.mock.calls[0][1];
+    expect(rejectMessageObject.message).toBe("Invalid JSON input for ad parameters");
 });
 
