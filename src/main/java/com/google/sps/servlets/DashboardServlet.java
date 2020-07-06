8c724ee12d81347e875201091b41d5e90ce0f909
Kaleab G <kaleabsolomon470@gmail.com>
2020-07-06 14:14:00 -0500

    Add html and js files

--- src/main/java/com/google/sps/servlets/DashboardServlet.java
+++ src/main/java/com/google/sps/servlets/DashboardServlet.java
@@ -20,24 +20,5 @@ public class DashboardServlet extends HttpServlet {
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
         
-    final String correlator = RequestUtils.getCorrelator(request);
-
-    if (correlator.isEmpty()) {
-      response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
-      return;
-    }
-
-    Entity interaction = new Entity(DBUtilities.INTERACTION_TABLE);
-    interaction.setProperty(Property.CORRELATOR, correlator);
-    interaction.setProperty(Property.FIND_NEAREST_LOCATION, false);
-    interaction.setProperty(Property.GRANTS_LOCATION, false);
-    interaction.setProperty(Property.INTERACTS_WITH_MAP, false);
-    interaction.setProperty(Property.SKIP_TO_CONTENT, false);
-    interaction.setProperty(Property.RETURN_TO_AD, false);
-
-    DatastoreService dataStore = DatastoreServiceFactory.getDatastoreService();
-    dataStore.put(interaction);
-
-    response.setStatus(HttpServletResponse.SC_OK); 
   }
 }
