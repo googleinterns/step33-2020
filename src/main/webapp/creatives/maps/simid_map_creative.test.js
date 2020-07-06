54b2dd447ea323fbf6671f09df524298e2df7943
kristen-m <kristenmason@google.com>
2020-07-06 12:31:01 -0500

    Add helper function to test file

--- src/main/webapp/creatives/maps/simid_map_creative.test.js
+++ src/main/webapp/creatives/maps/simid_map_creative.test.js
@@ -1,7 +1,30 @@
 /** Tests to see if Find Nearest X button is appending correct text */
 import SimidMapCreative from './simid_map_creative.js';
 
-let testMap, googleMaps;
+let testMap;
+let startData;
+
+/**
+ * Creates an object containing the test event data used in the SIMID protocol.
+ * @param {string=} adParameters Takes in the given string of AdParameters.
+ * If the field is left blank, passes in the default parameters.
+ * @return {!Object} an object containing the event data and corresponding AdParameters.
+ */
+function createEventData(adParameters = ""){
+    const eventData = {
+        args: {
+            creativeData: {
+                adParameters: '{'+adParameters+'}',
+            },
+            environmentData: {},
+        },
+        messageId: 0,
+        sessionId: "test-session-id",
+        timestamp: 0,
+        type: "SIMID:Player:init",
+    };
+    return eventData;
+}
 
 beforeEach(() => {
     window.google = {
@@ -16,29 +39,17 @@ beforeEach(() => {
     document.body.innerHTML = `
     <button id="findNearest"></button>
     <div id="map"></div>`;
-});
-
-test('testing button text updates from ad params', () => {
-    const eventData = {
-        args: {
-            creativeData: {
-                adParameters: '{"buttonLabel": "Place"}',
-            },
-            environmentData: {},
-        },
-        messageId: 0,
-        sessionId: "test-session-id",
-        timestamp: 0,
-        type: "SIMID:Player:init",
-    };
-    testMap.onInit(eventData);
-
-    const startData = {
+    startData = {
         messageId: 1,
         sessionId: "test-session-id",
         timestamp: 0,
         type: "SIMID:Player:startCreative",  
     }
+});
+
+test('testing button text updates from ad params', () => {
+    const eventData = createEventData('"buttonLabel": "Place"');
+    testMap.onInit(eventData);
     testMap.onStart(startData);
 
     const button = document.getElementById("findNearest");
@@ -46,58 +57,18 @@ test('testing button text updates from ad params', () => {
 });
 
 test('testing button text updates with default ad params', () => {
-    const eventData = {
-        args: {
-            creativeData: {
-                adParameters: '{}',
-            },
-            environmentData: {},
-        },
-        messageId: 0,
-        sessionId: "test-session-id",
-        timestamp: 0,
-        type: "SIMID:Player:init",
-    };
+    const eventData = createEventData();
     testMap.onInit(eventData);
-
-    const startData = {
-        messageId: 1,
-        sessionId: "test-session-id",
-        timestamp: 0,
-        type: "SIMID:Player:startCreative",  
-    }
     testMap.onStart(startData);
-
     const button = document.getElementById("findNearest");
     expect(button.innerText).toBe('Find Nearest Location');
 });
 
 test('instance of map is instantiated on button click', () => {
-    const eventData = {
-        args: {
-            creativeData: {
-                adParameters: '{}',
-            },
-            environmentData: {},
-        },
-        messageId: 0,
-        sessionId: "test-session-id",
-        timestamp: 0,
-        type: "SIMID:Player:init",
-    };
+    const eventData = createEventData();
     testMap.onInit(eventData);
-
-    const startData = {
-        messageId: 1,
-        sessionId: "test-session-id",
-        timestamp: 0,
-        type: "SIMID:Player:startCreative",  
-    }
     testMap.onStart(startData);
-
     const findNearestButton = document.getElementById('findNearest');
-    const mapDiv = document.getElementById("map");
     findNearestButton.dispatchEvent(new Event('click'));
     expect(window.google.maps.Map.mock.instances.length).toBe(1);
-
 });
