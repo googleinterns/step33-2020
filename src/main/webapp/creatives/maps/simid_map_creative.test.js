25ac0dc79f56f03c1760ad2146ccc28514616fc0
kristen-m <kristenmason@google.com>
2020-07-07 08:39:07 -0500

    Refactor for merge conflict

--- src/main/webapp/creatives/maps/simid_map_creative.test.js
+++ src/main/webapp/creatives/maps/simid_map_creative.test.js
@@ -48,29 +48,58 @@ beforeEach(() => {
 });
 
 test('testing button text updates from ad params', () => {
-    const eventData = createInitData('"buttonLabel": "Place"');
+    const eventData = {
+        args: {
+            creativeData: {
+                adParameters: '{"buttonLabel": "Place"}',
+            },
+            environmentData: {},
+        },
+        messageId: 0,
+        sessionId: "test-session-id",
+        timestamp: 0,
+        type: "SIMID:Player:init",
+    };
     testMap.onInit(eventData);
-    testMap.onStart(startData);
+
+    const buttonLabel = "Place";
+    const startData = {
+        messageId: 1,
+        sessionId: "test-session-id",
+        timestamp: 0,
+        type: "SIMID:Player:startCreative",  
+    }
+    testMap.onStart(startData, buttonLabel);
 
     const button = document.getElementById("findNearest");
     expect(button.innerText).toBe('Find Nearest Place');
 });
 
 test('testing button text updates with default ad params', () => {
-    const eventData = createInitData();
+    const eventData = {
+        args: {
+            creativeData: {
+                adParameters: '{}',
+            },
+            environmentData: {},
+        },
+        messageId: 0,
+        sessionId: "test-session-id",
+        timestamp: 0,
+        type: "SIMID:Player:init",
+    };
     testMap.onInit(eventData);
+
+    const startData = {
+        messageId: 1,
+        sessionId: "test-session-id",
+        timestamp: 0,
+        type: "SIMID:Player:startCreative",  
+    }
     testMap.onStart(startData);
+
     const button = document.getElementById("findNearest");
     expect(button.innerText).toBe('Find Nearest Location');
-});
-
-test('instance of map is instantiated on button click', () => {
-    const eventData = createInitData();
-    testMap.onInit(eventData);
-    testMap.onStart(startData);
-    const findNearestButton = document.getElementById('findNearest');
-    findNearestButton.dispatchEvent(new Event('click'));
-    expect(window.google.maps.Map.mock.instances.length).toBe(1);
 });
 
 test('marker is added to map when map loads', () => {
