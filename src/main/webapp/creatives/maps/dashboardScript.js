f7b2b49edf6748e599b23acd3e4233d8b2f6b7c4
Kaleab G <kaleabsolomon470@gmail.com>
2020-07-09 13:29:46 -0500

    Remove import in front-facing js

--- src/main/webapp/creatives/maps/dashboardScript.js
+++ src/main/webapp/creatives/maps/dashboardScript.js
@@ -1,12 +1,10 @@
 
-import {URL} from './utils.js'
-
 google.charts.load('current', {'packages':['corechart']});
 google.charts.setOnLoadCallback(drawChart);
 
 /** Fetches interaction data from server and uses it to create a chart. */
 function drawChart(parameter = "") {
-  fetch(`/${URL.DASHBOARD}?${parameter}`).then(response => response.json())
+  fetch(`/dashboard?${parameter}`).then(response => response.json())
   .then((dataPercentages) => {
     const data = new google.visualization.DataTable();
     data.addColumn('string', 'Interaction Type');
