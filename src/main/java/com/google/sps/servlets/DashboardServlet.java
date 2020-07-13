fb345c4b003a0996bdbd104dbdf7947b439fd039
Kaleab G <kaleabsolomon470@gmail.com>
2020-07-13 09:31:05 -0500

    Modularize dashboard servlet

--- src/main/java/com/google/sps/servlets/DashboardServlet.java
+++ src/main/java/com/google/sps/servlets/DashboardServlet.java
@@ -26,6 +26,8 @@ public class DashboardServlet extends HttpServlet {
   private final long MILLISECONDS_IN_DAY = 86400000;
   private final String DEFAULT_START_TIMESTAMP = "0";
   private final String DEFAULT_END_TIMESTAMP = String.valueOf(System.currentTimeMillis());
+  private String startTimestamp;
+  private String endTimestamp;
 
  /**
   * This route will return a JSON with the percentages of each interaction
@@ -33,26 +35,22 @@ public class DashboardServlet extends HttpServlet {
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
 
-    String startTimestamp = RequestUtils.getParameter(request, "startTime");
-    String endTimestamp = RequestUtils.getParameter(request, "endTime");
+    String requestStartTimestamp = RequestUtils.getParameter(request, "startTime");
+    String requestEndTimestamp = RequestUtils.getParameter(request, "endTime");
 
-    if (startTimestamp.isEmpty() || endTimestamp.isEmpty()) {
-      startTimestamp = DEFAULT_START_TIMESTAMP;
-      endTimestamp = DEFAULT_END_TIMESTAMP;
-    } else {
-      // endTimestamp should include the entire second date requested
-      endTimestamp = String.valueOf(Long.valueOf(endTimestamp) + MILLISECONDS_IN_DAY);
-    }
+    validateAndSetVariables(requestStartTimestamp, requestEndTimestamp);    
     
     try {
-      HashMap<String, Double> dataToSend = calculatePercentages(startTimestamp, endTimestamp);
+      HashMap<String, Double> dataToSend = calculatePercentages();
       
       String jsonToSend = convertToJson(dataToSend);
 
+      response.setStatus(HttpServletResponse.SC_OK);
       response.setContentType("application/json; charset=UTF-8");
       response.getWriter().println(jsonToSend);
     
     } catch (IllegalAccessException exception) {
+
       response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
     }
   }
@@ -65,11 +63,11 @@ public class DashboardServlet extends HttpServlet {
   * @param endTimestamp Timestamp for the end of the date range that is requested.
   * @return A hashmap with the interactions as keys and the percentages as values.
   */
-  public HashMap<String, Double> calculatePercentages(String startTimestamp, String endTimestamp) throws IllegalAccessException {
+  public HashMap<String, Double> calculatePercentages() throws IllegalAccessException {
     DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
 
-    final Filter startTimestampFilter =  new FilterPredicate(Property.TIMESTAMP, FilterOperator.GREATER_THAN_OR_EQUAL, startTimestamp);
-    final Filter endTimestampFilter =  new FilterPredicate(Property.TIMESTAMP, FilterOperator.LESS_THAN_OR_EQUAL, endTimestamp);
+    final Filter startTimestampFilter =  new FilterPredicate(Property.TIMESTAMP, FilterOperator.GREATER_THAN_OR_EQUAL, this.startTimestamp);
+    final Filter endTimestampFilter =  new FilterPredicate(Property.TIMESTAMP, FilterOperator.LESS_THAN_OR_EQUAL, this.endTimestamp);
     
     // combine the two filters with the 'and' operator 
     final Query timedQuery = new Query(DBUtilities.INTERACTION_TABLE).setFilter(CompositeFilterOperator.and(startTimestampFilter, endTimestampFilter));
@@ -80,7 +78,7 @@ public class DashboardServlet extends HttpServlet {
   
     HashMap<String, Double> dataToSend = new HashMap<>();
 
-    // start iterating at 2 to skip timestamp and correlator properties.
+    // start iterating at 2 to skip timestamp and correlator properties
     for (int i = 2; i < allFields.length; ++i){
       String property = (String) allFields[i].get(classInstance); // gets the value of the field variable
 
@@ -112,5 +110,30 @@ public class DashboardServlet extends HttpServlet {
     Gson gson = new Gson();
     String json = gson.toJson(data);
     return json;
+  }
+
+ /**
+  * Given the start and end timestamps requested, this function validates and sets 
+  * the instance variables.
+  *
+  * @param startTimestamp A string containing the requested start time as a UNIX timestamp
+  * @param endTimestamp A string containing the requested end time as a UNIX timestamp
+  */
+  private void validateAndSetVariables(String startTimestamp, String endTimestamp){
+
+    if (startTimestamp.isEmpty() || endTimestamp.isEmpty()) {
+      this.startTimestamp = DEFAULT_START_TIMESTAMP;
+      this.endTimestamp = DEFAULT_END_TIMESTAMP;
+    
+    } else {  
+      // swap if they're in the wrong order
+      if (Long.valueOf(endTimestamp) < Long.valueOf(startTimestamp)) {
+        this.startTimestamp = endTimestamp;
+        this.endTimestamp = startTimestamp;
+      }
+      // endTimestamp should include the entire second date requested
+      this.endTimestamp = String.valueOf(Long.valueOf(this.endTimestamp) + MILLISECONDS_IN_DAY);
+    }
+
   }
 }
