2a765d41e0178abebc328177d66ac2a574b4ae53
Kaleab G <kaleabsolomon470@gmail.com>
2020-07-13 16:43:52 -0500

    Finish test for dashboard

--- src/test/java/com/google/sps/servlets/DashboardServletTest.java
+++ src/test/java/com/google/sps/servlets/DashboardServletTest.java
@@ -15,6 +15,15 @@ import com.google.appengine.tools.development.testing.LocalDatastoreServiceTestC
 import com.google.appengine.tools.development.testing.LocalServiceTestHelper;
 import java.io.StringWriter;
 import java.io.PrintWriter;
+import com.google.appengine.api.datastore.Query;
+import com.google.sps.servlets.Property;
+import com.google.appengine.api.datastore.Query.Filter;
+import com.google.appengine.api.datastore.Query.FilterPredicate;
+import com.google.appengine.api.datastore.Query.FilterOperator;
+import com.google.appengine.api.datastore.Query.CompositeFilterOperator;
+import com.google.appengine.api.datastore.DatastoreService;
+import com.google.appengine.api.datastore.DatastoreServiceFactory;
+import com.google.appengine.api.datastore.Entity;
 
 @RunWith(JUnit4.class)
 public final class DashboardServletTest {
@@ -64,34 +73,31 @@ public final class DashboardServletTest {
   }
 
   @Test
-  public void testPercentageMethodCalledWithCorrectOrder() throws IOException, IllegalAccessException {
+  public void testCorrectNumberReturnedOnQuery() throws IOException, IllegalAccessException {
 
     Mockito.when(request.getParameter("startTime")).thenReturn("0");
     Mockito.when(request.getParameter("endTime")).thenReturn("1");
     Mockito.when(response.getWriter()).thenReturn(printWriter);
 
-    // DashboardServlet dashboardServletObject = new DashboardServlet();
-    // dashboardServletObject.doGet(request, response);
+    Filter startTimestampFilter =  new FilterPredicate(Property.TIMESTAMP, FilterOperator.GREATER_THAN_OR_EQUAL,request.getParameter("startTime"));
+    Filter endTimestampFilter =  new FilterPredicate(Property.TIMESTAMP, FilterOperator.LESS_THAN_OR_EQUAL, request.getParameter("endTime"));
+    
+    Query timedQuery = new Query("Interactions").setFilter(CompositeFilterOperator.and(startTimestampFilter, endTimestampFilter));
+    DatastoreService dataStore = DatastoreServiceFactory.getDatastoreService();
 
-    DashboardServlet ds = Mockito.mock(DashboardServlet.class);
-    ds.doGet(request, response);
+    Entity originalInteraction = new Entity("Interactions");
+    originalInteraction.setProperty(Property.CORRELATOR, "Person1");
+    originalInteraction.setProperty(Property.FIND_NEAREST_LOCATION, false);
+    originalInteraction.setProperty(Property.GRANTS_LOCATION, false);
+    originalInteraction.setProperty(Property.INTERACTS_WITH_MAP, false);
+    originalInteraction.setProperty(Property.SKIP_TO_CONTENT, false);
+    originalInteraction.setProperty(Property.RETURN_TO_AD, false);
+    originalInteraction.setProperty(Property.TIMESTAMP, "1");
 
-    Mockito.verify(ds).calculatePercentages();
-  }
+    dataStore.put(originalInteraction);
 
-  @Test
-  public void testPercentageMethodWithSwappedOrder() throws IOException, IllegalAccessException {
+    new DashboardServlet().doGet(request, response);
 
-    Mockito.when(request.getParameter("startTime")).thenReturn("1");
-    Mockito.when(request.getParameter("endTime")).thenReturn("0");
-    Mockito.when(response.getWriter()).thenReturn(printWriter);
-
-    // DashboardServlet dashboardServletObject = new DashboardServlet();
-    // dashboardServletObject.doGet(request, response);
-
-    DashboardServlet ds = Mockito.mock(DashboardServlet.class);
-    ds.doGet(request, response);
-
-    Mockito.verify(ds).calculatePercentages();
+    Assert.assertEquals(1, dataStore.prepare(timedQuery).countEntities());
   }
 }
