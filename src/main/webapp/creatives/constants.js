e556beef8582ec08755c34d454a53557e0cc989e
Kaleab G <kaleabsolomon470@gmail.com>
2020-07-15 12:01:35 -0500

    Add urls file to constants

--- src/main/webapp/creatives/constants.js
+++ src/main/webapp/creatives/constants.js
@@ -100,3 +100,14 @@ export const CreativeErrorCode = {
   MESSAGES_NOT_FOLLOWING_SPEC: 1110,
   PLAYER_RESPONSE_TIMEOUT: 1111,
 };
+
+/** A list of server routes that keep track of each interaction when fetched. */
+export const Urls = {
+  DEFAULT_BASE_URL: "https://step-capstone-team33-2020.appspot.com/",
+  INITIALIZE: "initialize",
+  FIND_NEAREST_LOCATION: "find-location",
+  GRANT_LOCATION_DATA: "grant-location",
+  MAP_INTERACT: "map-interact",
+  SKIP_TO_CONTENT: "skip-to-content",
+  RETURN_TO_AD: "return-to-ad",
+}
